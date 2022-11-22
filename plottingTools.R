57ca23265ea50ead032d05b0d677f293be0ea1e0
Oliver De Sa <oliver.desa@mail.utoronto.ca>
2022-11-22 15:37:56 -0500

    added code that places significance values between different groups

--- plottingTools.R
+++ plottingTools.R
@@ -9,10 +9,16 @@ library(ggsignif)
 mouse_data <- read.csv('141122GBRepeat(IL8).csv')
 
 Boxplot <- function(dataframe){
-  ggplot(data = dataframe, aes(x = Day, y = Corrected.OD655, fill = 'Treatment')) +
-    geom_boxplot()+
-    geom_signif(comparisons = list(c('D14', 'D30')),
-                map_signif_level = TRUE, na.rm = TRUE)
+  sig_test <- function
+  
+  ggplot(data = dataframe, aes(x = Day, y = Corrected.OD655, fill = Treatment)) +
+    geom_boxplot() +
+    geom_signif(
+      annotations = c('*', '*'),
+      y_position = c(0.55, 0.5), xmin = c(0.8, 1.8), xmax = c(1.2, 2.2)) +
+      
+    #geom_signif(comparisons = list(c('D14', 'D30')), 
+                #y_position = 0.5, tip_length = 0, vjust = 0.2)+
     labs(x = 'Day Post Treatment',
          y = 'Response Ratio (650nm)',
          title = 'Effect of SaGA Supplementation on MDP Levels of Fecal Samples',
