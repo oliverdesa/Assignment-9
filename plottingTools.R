16b13001251f42e2c5d7b1fd42bb8d5ba0888238
Oliver De Sa <oliver.desa@mail.utoronto.ca>
2022-11-24 19:26:33 -0500

    added comments to the plotting tools file to describe what the plots are showing and how they were generated

--- plottingTools.R
+++ plottingTools.R
@@ -6,7 +6,34 @@
 library(ggplot2)
 library(ggsignif)
 
+
+# This function takes 2 groups, the Day 14 and Day 30 groups, and then splits them 
+# by treatment type to visualize the differences between the 2 treatments at
+# different time points post treatment.
+
 Boxplot <- function(dataframe){
+  
+  # Disclaimer: I calculated the significance values plotted on this graph using
+  # the pandas and scipy packages in python. Because I had to manually add the 
+  # significance values to the plot anyway I didnt assess these stats here, 
+  # but I will reference the code used in python to calculate:
+  
+  ## T test
+  
+  # pbsD14 = np.array(SagAsupplement.iloc[0:4,1])
+  # pbsD30 = np.array(SagAsupplement.iloc[4:8,1])
+  # 
+  # SagAD14 = np.array(SagAsupplement.iloc[8:12,1])
+  # SagAD30 = np.array(SagAsupplement.iloc[12:16,1])
+  
+  # Check for closely related variances
+  # print(np.var(pbsD14), np.var(SagAD14))
+  # print(np.var(pbsD30), np.var(SagAD30))
+  # 
+  # Run 2 sample unpaired T-test w/ equivalent variances 
+  # print(stats.ttest_ind(a=pbsD14, b=SagAD14, equal_var=True))
+  # print(stats.ttest_ind(a=pbsD30, b=SagAD30, equal_var=True))
+  
   ggplot(data = dataframe, aes(x = Day, y = Corrected.OD655, fill = Treatment)) +
     geom_boxplot() +
     geom_signif(
@@ -18,6 +45,11 @@ Boxplot <- function(dataframe){
     theme_bw()
   ggsave('Boxplot.pdf', scale = 2)
 }
+
+# This function plots the Optical Density at 655nm depending on the amount of 
+# stimulant added to the assay in ng/ml. A quadratic fit is then applied to the
+# generated standard curve that allows for the approximation of stimulant concentration
+# for each sample depending on its respective optical density.
 
 Scatter_quadratic <- function(dataframe){
   ggplot(data = dataframe, aes(x = Conc, y = OD)) +
